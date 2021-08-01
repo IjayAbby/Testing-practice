9dcb42a5e040760b5ad1af563f7125ca68c668f5
IjayAbby <inyangasi4@gmail.com>
2021-08-01 04:57:25 +0300

    Calculator class

--- testing.js
+++ testing.js
@@ -15,8 +15,51 @@ const reverseString = (string) => {
     return string.split('').reverse().join('');
 }
 
+//Task 3
+class calculator {
+    static add(number1, number2) {
+        if (typeof number1 === 'string' || typeof number2 === 'string') {
+            throw new Error("Some arguments are not numbers");
+        } else if (!number1 || !number2) {
+            return undefined;
+        }
+        return number1 + number2;
+    }
+
+    static subtract(number1, number2) {
+        if (typeof number1 === 'string' || typeof number2 === 'string') {
+            throw new Error("Some arguments are not numbers");
+        } else if (!number1 || !number2) {
+            return undefined;
+        }
+        return number1 - number2;
+    }
+
+    static divide(number1, number2) {
+        if (typeof number1 === 'string' || typeof number2 === 'string') {
+            throw new Error("Some arguments are not numbers");
+        } else if (!number1 || !number2) {
+            return undefined;
+        }
+        return number1 / number2;
+    }
+
+    static multiply(number1, number2) {
+        if (typeof number1 === 'string' || typeof number2 === 'string') {
+            throw new Error("Some arguments are not numbers");
+        } else if (!number1 || !number2) {
+            return undefined;
+        }
+        return number1 * number2;
+    }
+};
+
 //Task 4
 
-const capitalizeFirstLetter(string) {
+const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
-}
+};
+
+const fctn = {stringLength, reverseString, capitalizeFirstLetter, calculator};
+
+module.exports = fctn;
